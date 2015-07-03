6693bca6601f3e587b394af318b1ae349c894584
unknown <walter.silva@WABHZPC093.webaula.local>
2015-07-03 10:23:22 -0300

    changes

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 ﻿/// -----------------------------------------------------------------------------------------------------------
 ///										SERVIDOR DE CHAT WEBAULA
 /// -----------------------------------------------------------------------------------------------------------
-var port			  = process.env.PORT || 3000;											// Porta do servidor
-var express			  = require('express');								// Instancia do framework Express
-var app				  = express();										// Inicialização do framework
-var http			  = require('http').createServer(app);				// Instancia do protocolo HTTP
-var util			  = require('util');								// Utilitário para coletar informações do servidor.
-var v8memoryUsage	  = util.inspect(process.memoryUsage().rss);		// Quantidade de memória alocada para o serviço
-var routes			  = require('./routes');							// Controle das rotas
-var path              = require('path');								// Utilitario para path.join
-var ExpressPeerServer = require('peer').ExpressPeerServer;
+var port				= process.env.PORT || 3000;						// Porta do servidor
+var express				= require('express');							// Instancia do framework Express
+var app					= express();									// Inicialização do framework
+var util				= require('util');								// Utilitário para coletar informações do servidor.
+var v8memoryUsage		= util.inspect(process.memoryUsage().rss);		// Quantidade de memória alocada para o serviço
+var routes				= require('./routes');							// Controle das rotas
+var path				= require('path');								// Utilitario para path.join
+var ExpressPeerServer	= require('peer').ExpressPeerServer;			// Peer-to-Peer connector
+var http = require('http').createServer(app);							// Instancia do protocolo HTTP
+
 // -----------------------------------------------------------------------------------------------------------
 // Inicia a escuta do servidor HTTP 
 // -----------------------------------------------------------------------------------------------------------
@@ -23,12 +24,14 @@ app.set('port', port);
 app.set('views', path.join(__dirname, 'views'));
 app.use('/', routes);
 app.use(express.static(path.join(__dirname, 'public')));
-
+// -----------------------------------------------------------------------------------------------------------
+// Configurações do Peer-to-Peer 
+// -----------------------------------------------------------------------------------------------------------
 var options = {
     debug: true
 }
+//
 app.use('/p2p', ExpressPeerServer(http, options));
-
 // -----------------------------------------------------------------------------------------------------------
 // Seta o número de conexões para ilimitado
 // -----------------------------------------------------------------------------------------------------------
